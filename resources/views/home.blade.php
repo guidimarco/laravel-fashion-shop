bcc46bc1594358c76b52af08cc7fe950bf3fa6f9
Marco Guidi <mark.88g@gmail.com>
2021-01-19 15:23:53 +0100

    ++ creata homepage.blade + creata rotta da controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>HOME PAGE</h1>
+        <a href="#">Vai alla pagina dei vestiti</a>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Fashon Shop</title>
+
+        <!-- css -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+
+        <main>
+            @yield('content')
+        </main>
+
+        <!-- js -->
+        <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+    </body>
+</html>
